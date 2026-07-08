cf7bcce8831920fe8b12977c31672d9b7a4a45b3
thyagorve <thyagolyma2015@hotmail.com>
2026-07-07 23:44:34 -0300

    ajuste troca de logo, e ajuste no mapa5

--- sementes/urls.py
+++ sementes/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,9 @@ urlpatterns = [
     path('', include('sapp.urls')), 
     path('almoxarifado/', include('almoxarifado.urls', namespace='almoxarifado')),
 ]
+
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
