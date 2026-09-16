581a200cda187a8a5bfc8f77918344a12876e9b6
thyagorve <thyagolyma2015@hotmail.com>
2026-09-16 09:49:52 -0300

    ajuste no infiniti fila3

--- sapp/static/js/service-worker.js
+++ sapp/static/js/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'infinity-stock-v11-shell-2';
+const CACHE_NAME = 'infinity-stock-v11-shell-3';
 const STATIC_FALLBACKS = [
   '/static/manifest.webmanifest',
   '/static/img/logo.png',
